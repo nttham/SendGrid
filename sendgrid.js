687c9924edfb5158911c8b13ed819955dbd41692
nttham <Neethu.TThampi@cognizant.com>
2016-05-26 18:24:40 +0530

    updated the comments
    authToken

--- sendgrid.js
+++ sendgrid.js
@@ -2,8 +2,11 @@
  * Created by 423919 on 5/23/2016.
  * This module is used to send sms
  */
+ 
+ //deependency
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var config = require('./config.json');
 
 var Sendgrid = function () {
 
@@ -11,9 +14,12 @@ var Sendgrid = function () {
 
 // this api will configure the twilio obj and send the sms
 Sendgrid.prototype.sendMail = function (app) {
-
+    //route to send mail
     app.post('/sendmail', jsonParser, function (req, res) {
+        //creating options for sendgrid api
         var message = {
+            "accountSID": config.sendgrid.accountSID,
+            "authToken": config.sendgrid.authToken,
             "toRecipient": req.body.toRecipient,
             "fromMail": req.body.fromMail,
             "subject": req.body.subject,
